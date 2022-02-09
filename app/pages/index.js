bc7ba1eab548ff52c8c073c533d2d591d8eeaf27
Noudea <oudea.nicolas@gmail.com>
2022-02-09 09:29:48 +0100

    :bug: build bug link referer

--- app/pages/index.js
+++ app/pages/index.js
@@ -148,11 +148,11 @@ export default function Home() {
         {isMinting && <p>minting nft please wait</p>}
         {hasMinted && 
           <div className="nft preview">
-            <p>Your nft is avaible <a href={`https://rinkeby.rarible.com/token/0xa33c278a6B38d439B5BD1Ec55440136046e6CfB7:${tokenId}`} target="_blank">here</a>, it can take up to 5 minutes to see your nft</p>
+            <p>Your nft is avaible <a rel="noreferrer" href={`https://rinkeby.rarible.com/token/0xa33c278a6B38d439B5BD1Ec55440136046e6CfB7:${tokenId}`} target="_blank">here</a>, it can take up to 5 minutes to see your nft</p>
           </div>
         }
         <div>
-          <a  target="_blank" href='https://rinkeby.rarible.com/collection/0xa33c278a6b38d439b5bd1ec55440136046e6cfb7/items'><svg width="20" height="20" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="40" height="40" rx="8" fill="#FEDA03"></rect><path d="M27.6007 19.8536C28.8607 19.5262 29.9817 18.5838 29.9817 16.6889C29.9817 13.5342 27.3031 12.8 23.8706 12.8H10.2V27.0064H15.9539V22.185H22.7793C23.8309 22.185 24.446 22.6016 24.446 23.6334V27.0064H30.2V23.4548C30.2 21.5203 29.1087 20.3 27.6007 19.8536ZM22.8785 18.3556H15.9539V16.9667H22.8785C23.6325 16.9667 24.0888 17.0659 24.0888 17.6612C24.0888 18.2564 23.6325 18.3556 22.8785 18.3556Z" fill="black"></path></svg> View Collection on Rarible</a>
+          <a  target="_blank" rel="noreferrer" href='https://rinkeby.rarible.com/collection/0xa33c278a6b38d439b5bd1ec55440136046e6cfb7/items'><svg width="20" height="20" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="40" height="40" rx="8" fill="#FEDA03"></rect><path d="M27.6007 19.8536C28.8607 19.5262 29.9817 18.5838 29.9817 16.6889C29.9817 13.5342 27.3031 12.8 23.8706 12.8H10.2V27.0064H15.9539V22.185H22.7793C23.8309 22.185 24.446 22.6016 24.446 23.6334V27.0064H30.2V23.4548C30.2 21.5203 29.1087 20.3 27.6007 19.8536ZM22.8785 18.3556H15.9539V16.9667H22.8785C23.6325 16.9667 24.0888 17.0659 24.0888 17.6612C24.0888 18.2564 23.6325 18.3556 22.8785 18.3556Z" fill="black"></path></svg> View Collection on Rarible</a>
         </div>
     
     </div>
